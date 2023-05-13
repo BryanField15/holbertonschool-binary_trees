4797636b6ba760c40014b35be3e8f135573081cf
BryanField15 <6367@holbertonstudents.com>
2023-05-13 03:37:54 -0700

    task 0 solution

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node
+ * @parent: pointer to parent node
+ * @value: int to be inserted into node
+ *
+ * Return: pointer to new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(*new_node));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	return (new_node);
+}
