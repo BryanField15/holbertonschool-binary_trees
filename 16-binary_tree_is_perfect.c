198f95b87a52bfa9a83e5d7ab210abf5b40d6158
BryanField15 <6367@holbertonstudents.com>
2023-05-15 03:34:11 -0700

    added recursive return call

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,6 +59,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	else
-		return (1);
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
 }
