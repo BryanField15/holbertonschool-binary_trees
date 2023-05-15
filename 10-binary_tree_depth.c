9e99cf4e6db89d77349932462e2ee45ff8ab9ef1
BryanField15 <6367@holbertonstudents.com>
2023-05-14 19:26:49 -0700

    typo

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * binary_tree_depth - measures the depth of a binary tree
+ * @tree: pointer to the root node of the tree to measure the depth
  * Return: depth of tree or 0 if NULL
  */
 
