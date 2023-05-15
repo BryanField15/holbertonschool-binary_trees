dff54f841634e138be5ea0feed3f3694f3f0508e
BryanField15 <6367@holbertonstudents.com>
2023-05-14 19:09:25 -0700

    added base case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,13 +16,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	if (height_left >= height_right)
+	if (height_left > height_right)
 	{
-		return (height_left + 1);
+		return (1 + height_left);
 	}
 	else
-		return (height_right + 1);
+		return (1 + height_right);
 }
