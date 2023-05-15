61d2b43d97bba40a2a3e006d4056d57b03789592
BryceKnight16 <6373@holbertonstudents.com>
2023-05-14 21:16:23 -0700

    moving the fuction into the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * _height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
+ */
+static size_t _height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + _height(tree->left) : 0;
+	height_r = tree->right ? 1 + _height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
  * binary_tree_balance - Measures balance of a binary tree
  * @tree: Pointer to the root node of the tree
  *
