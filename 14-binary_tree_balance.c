78d2a3cfa0d3b71635582015931e3db010e177d3
BryceKnight16 <6373@holbertonstudents.com>
2023-05-14 21:14:17 -0700

    changing called function name to match the function i need

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,8 +16,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = _height(tree->left);
+	right_height = _height(tree->right);
 
 	return (left_height - right_height);
 }
